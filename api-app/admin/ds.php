0d7adda9312ff6763f5711adcbfc854b7304a661
arisfebriyanto193 <arisfebriyanto193@gmail.com>
2026-07-25 10:54:43 +0700

    oko

--- api-app/admin/ds.php
+++ api-app/admin/ds.php
@@ -352,7 +352,10 @@ if ($method === 'POST') {
             echo json_encode(["status"=>false, "message"=>"Gagal insert device: ".$conn->error]); exit();
         }
 
-        // Insert Params
+        $is_demo = !empty($input['is_demo']) ? true : false;
+
+        if (!$is_demo) {
+            // Insert Params
         if(isset($input['params']) && is_array($input['params'])){
             foreach($input['params'] as $idx => $p){
                 $label = mysqli_real_escape_string($conn, $p['label']);
@@ -395,7 +398,7 @@ if ($method === 'POST') {
         if(strtoupper($dev_type) == 'AWLR') {
              $conn->query("INSERT INTO device_settings (device_unique_id, parameter_name,  tinggi_sensor, unit, is_visible, category, mqtt_topic) VALUES ('$dev_id', 'tuc', '400', '1', 0, 'config', 'config')");
              $conn->query("INSERT INTO device_settings (device_unique_id, parameter_name,  tinggi_sensor, unit, is_visible, category, mqtt_topic) VALUES ('$dev_id', 'tuc', '400', '1', 0, 'sungai', 'jenis')");
-        }
+        } // end if (!$is_demo)
 
         echo json_encode(["status"=>true, "message"=>"User berhasil dibuat"]);
         exit();
